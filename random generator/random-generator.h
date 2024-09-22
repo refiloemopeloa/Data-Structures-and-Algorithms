320dcfad355fab4b45b2b24a36acaa5cb35d3b71
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2024-09-22 23:09:39 +0200

    created function for swapping elementd

---
+++ random generator/random-generator.h
@@ -0,0 +1,15 @@
+//
+// Created by matth on 2024/08/26.
+//
+
+#ifndef RANDOM_GENERATOR_H
+#define RANDOM_GENERATOR_H
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+#endif //RANDOM_GENERATOR_H
