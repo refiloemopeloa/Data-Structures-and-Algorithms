4b5cc57d55811bb05f2fca2f5666a6f1d81c29eb
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-23 01:39:42 +0200

    main program where input is accepted

---
+++ sorting/bubble sort/main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by matth on 2024/09/22.
+//
+
+#include "bubble_sort.h"
+#include "stdio.h"
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    char* input=NULL;
+    size_t buffer_size = 0;
+    printf("Please enter how many elements you would like to enter:\n");
+    int input_size = getline(&input, &buffer_size, stdin);
+    int num_elements = atoi(input);
+    int array[num_elements];
+
+    for (int i=0; i<num_elements;i++) {
+        input_size = getline(&input, &buffer_size, stdin);
+        array[i] = atoi(input);
+    }
+
+    bubble_sort(array,num_elements);
+
+    return 0;
+}
