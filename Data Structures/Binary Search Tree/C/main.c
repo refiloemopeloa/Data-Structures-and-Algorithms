20ca770c2f0d25a15d4e924ba8ee0e2b50a66ad0
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-10-08 01:02:38 +0200

    Implemented BST struct, and Node constructor function

--- Data Structures/Binary Search Tree/C/main.c
+++ Data Structures/Binary Search Tree/C/main.c
@@ -10,7 +10,27 @@ typedef struct Node
     char visited;
 } Node;
 
-int main() {
+#define nullptr ((void *)0);
 
+typedef struct
+{
+    Node *root;
+} BST;
+
+void new_Node(Node *this, Node *left, Node *right, Node *parent, int *key)
+{
+    this->left = left;
+    this->right = right;
+    this->parent = parent;
+    this->key = key;
+    this->visited = 0;
+}
+
+void new_BST(BST *this, int *array, int *size)
+{
+    ;
+}
+
+int main() {
     return 0;
 }
